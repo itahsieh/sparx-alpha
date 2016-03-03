77818c08b0ed48b6c7f94cc3b74190ee8a9ee21b
I-Ta Hsieh <friendshipspy@gmail.com>
2016-03-03 18:00:03 +0800

    the bug of central pixel in FITS' header is fixed

--- src/sparx-task-telsim.c
+++ src/sparx-task-telsim.c
@@ -1876,13 +1876,13 @@ static void FITSoutput( char *FileName, const char *bunit, double scale, int Sto
         sprintf(cellscal,"1/F     ");
         
         double 
-	crpix1 = (double) naxes[0]/2. + 0.5,
-	crpix2 = (double) naxes[1]/2. + 0.5,
-	crpix3 = (double) naxes[2]/2. + 0.5,
+	crpix1 = (double) (naxe[0]/2 +1),
+	crpix2 = (double) (naxe[1]/2 +1),
+	crpix3 = (double) naxes[2]/2.+1.,
 	crpix4 = 1.,
 	cdelt1 = 180. / M_PI * glb.x.delt,
 	cdelt2 = 180. / M_PI * glb.y.delt,
-	cdelt3 = 180. / M_PI * glb.v.delt,
+	cdelt3 = 0.001 * glb.v.delt,
 	cdelt4 = 1.,
 	crval = 0.0,
 	crval4 = 1.,
@@ -1919,8 +1919,7 @@ static void FITSoutput( char *FileName, const char *bunit, double scale, int Sto
 	fits_write_key(fptr,TDOUBLE,"BMIN",&beam,"Minor beam axis ",&status);
 	fits_write_key(fptr,TDOUBLE,"BPA",&beam,"PA",&status);
 	fits_write_key(fptr,TDOUBLE,"BSCALE",&bscale,"",&status);
-	fits_write_key(fptr,TDOUBLE,"BZERO",&bzero,"",&status);
-	fits_write_key(fptr,TDOUBLE,"RESTFREQ",&restfreq,"",&status);
+	fits_write_key(fptr,TDOUBLE,"BZERO",&bzero,"",&status);   	fits_write_key(fptr,TDOUBLE,"RESTFREQ",&restfreq,"",&status);
 	fits_write_key(fptr,TSTRING,"BUNIT",bunit,"",&status);
 	fits_write_key(fptr,TSTRING,"CELLSCAL",&cellscal,"",&status);
         Deb_ASSERT(status == 0);
