9483545a5931c9a3c91713b9e450f1a2d23919ab
I-Ta Hsieh <vandine@localhost.localdomain>
2016-09-08 09:29:33 +0800

    fedora 22/24 porting

--- fedora-install.sh
+++ fedora-install.sh
@@ -1,9 +1,33 @@
 #! /bin/bash
 
+FEDORA_VERSION=`grep -o '[0-9]*' /etc/fedora-release`
+
+
+if [ $FEDORA_VERSION == '22' ]; then 
+
+# INSTALL SPARX FOR FEDORA 22
+sudo yum install \
+redhat-rpm-config \
+python-devel \
+numpy \
+gsl-devel \
+libtool \
+fftw-devel \
+cfitsio-devel \
+hdf5-openmpi-devel \
+libX11-devel \
+python2-matplotlib \
+scipy \
+sympy \
+python-tables
+
+elif [ $FEDORA_VERSION == '24' ]; then
+
 # INSTALL SPARX FOR FEDORA 24
 sudo yum install \
 redhat-rpm-config \
 python-devel \
+numpy \
 gsl-devel \
 libtool \
 fftw-devel \
@@ -15,18 +39,19 @@ python2-scipy \
 sympy \
 python-tables
 
-HDF_INCLUDE='--with-include=/usr/include/hdf5/openmpi'
+fi
+
 HDF_LIB='--with-lib=/usr/lib64/openmpi/lib'
 HDF_INCLUDE='--with-include=/usr/include/openmpi-x86_64'
 FITSIO_INCLUDE='--with-include=/usr/include/cfitsio'
+
 
 destination=$HOME/opt/sparx
 rm -rf build/* $destination
 
 python setup.py install --prefix=$destination \
-$HDF_INCLUDE \
-$FITSIO_INCLUDE \
-$HDF_LIB
+$HDF_INCLUDE $HDF_LIB \
+$FITSIO_INCLUDE
 
 
 if ! grep -q "# SPARX PATH" ~/.bashrc; then
