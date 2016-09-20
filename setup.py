b3f1c34b3b8eea96031f95ba08af611f739c6103
itahsieh <ithsieh@cp2.asiaa.sinica.edu.tw>
2016-09-20 12:34:57 +0800

    linux porting update

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ SPARX_VERSION='sparx'
 USER_INCLUDE = []
 USER_LIB = []
 args = sys.argv[:]
-mpi_libs = []
+mpi_libs = ['mpi']
 for arg in args:
 	if arg.find('--with-include=') == 0:
 		USER_INCLUDE += [expanduser(arg.split('=')[1])]
