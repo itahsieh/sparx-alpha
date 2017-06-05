21f40de67d7fe755839e0c123fb5c8d3f9e59a38
I-Ta Hsieh <ita.hsieh@gmail.com>
2017-06-05 09:51:12 +0800

    tiara module fallback

--- load_tiara_module.sh
+++ load_tiara_module.sh
@@ -3,15 +3,21 @@
 . /etc/profile.d/modules.sh
 module purge
 
-module add icc/16.0
 module add cfitsio
-module add openmpi/2.0.1_ic16.0
-module add fftw/3.3.4_openmpi_2.0.1_ic16.0
 module add python/2.7.11
-module add hdf5/1.8.16_openmpi_2.0.1_ic16.0 
-module add gsl/2.1_ic16.0
 module add miriad
 
+# module add openmpi/2.0.1_ic16.0
+# module add fftw/3.3.4_openmpi_2.0.1_ic16.0
+# module add hdf5/1.8.16_openmpi_2.0.1_ic16.0 
+# module add gsl/2.1_ic16.0
+# module add icc/16.0
+
+module add hdf5/1.8.16_openmpi_1.10.2_ic15.0
+module add openmpi/1.10.2_ic15.0
+module add icc/15.0
+module add module add gsl/2.1_ic15.0
+module add fftw/3.3.4_openmpi_1.10.2_ic15.0
 
 
 HOSTNAME=`hostname`
