06eaf51251927583d358b0f5679c8076af0c4077
I-Ta Hsieh <ita.hsieh@gmail.com>
2017-10-20 16:25:13 +0800

    alpha-3.1 commitment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Python Distutils setup script for SPARX
 
 # VERSION_NUMBER of SPARX
-VERSION_NUMBER = '3.0.6'
+VERSION_NUMBER = '3.1'
 
 # number of Thread using in per job
 ENABLE_MULTITREADING = 1
