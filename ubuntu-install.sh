200c4f912a9075fadb0ae8f32540b0ea84a0d45c
I-Ta Hsieh <ita.hsieh@gmail.com>
2019-01-03 19:22:39 +0800

    ubuntu 18.04 support

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,24 +1,42 @@
 #! /bin/bash
 
-# INSTALL SPARX FOR UBUNTU
+CODENAME=`lsb_release -c | awk 'END {print $NF}'`
+
+# INSTALL SPARX DEPENDENCIES ON UBUNTU
+if [ $CODENAME == 'bionic' ] || [ $CODENAME == 'xenial' ] || [ $CODENAME == 'trusty' ]; then
+	echo 'UBUNTU VERSION:'$CODENAME
+else
+	echo 'unsupported ubuntu version:'$CODENAME
+	exit 1
+fi
+
 sudo apt-get install \
-libpython2.7-dev \
-libgsl0-dev \
-libtool \
-libfftw3-dev \
-libcfitsio3-dev \
-libhdf5-openmpi-dev \
-libx11-dev \
-python-matplotlib \
-python-sympy \
-python-tables
+	libpython2.7-dev \
+	libgsl0-dev \
+	libtool \
+	libfftw3-dev \
+	libhdf5-openmpi-dev \
+	libx11-dev \
+	python-matplotlib \
+	python-sympy \
+	python-tables
+
+if [ $CODENAME == 'bionic' ]; then
+	sudo apt-get install \
+		libcfitsio-dev \
+		libopenmpi-dev
+elif [ $CODENAME == 'xenial' ] || [ $CODENAME == 'trusty' ]; then
+	sudo apt-get install libcfitsio3-dev
+fi
 
 
+# ENVIRONMENT VARIABLES
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:/usr/include/hdf5/openmpi:/usr/lib/openmpi/include
 export LIBRARY_PATH=$LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/hdf5/openmpi
 
-CODENAME=`lsb_release -c | awk 'END {print $NF}'`
-if   [ $CODENAME == 'xenial' ]; then
+if [ $CODENAME == 'bionic' ]; then
+  MPI_INCLUDE='--with-include=/usr/lib/x86_64-linux-gnu/openmpi/include'
+elif [ $CODENAME == 'xenial' ]; then
   HDF_INCLUDE='--with-include=/usr/include/hdf5/openmpi'
   MPI_INCLUDE='--with-include=/usr/lib/openmpi/include'
   HDF_LIB='--with-lib=/usr/lib/x86_64-linux-gnu/hdf5/openmpi'
@@ -27,6 +45,7 @@ elif [ $CODENAME == 'trusty' ]; then
 fi
 
 
+# SPARX INSTALLATION
 destination=$HOME/opt/sparx
 rm -rf build/* $destination
 
@@ -38,6 +57,7 @@ $MPI_INCLUDE \
 $HDF_LIB
 
 
+# BASHRC APPENDIND
 if ! grep -q "# SPARX PATH" ~/.bashrc; then
   echo "# SPARX PATH" >> ~/.bashrc
   echo 'PATH=$PATH:$HOME/opt/sparx/bin' >> ~/.bashrc
