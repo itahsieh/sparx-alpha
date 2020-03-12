b4124bd66784f62017e64ddbfa13589f53945e1f
I-Ta Hsieh <friendshipspy@gmail.com>
2020-03-12 15:50:18 +0800

    alpha-3.1.3 tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Python Distutils setup script for SPARX
 
 # VERSION_NUMBER of SPARX
-VERSION_NUMBER = '3.1.2'
+VERSION_NUMBER = '3.1.3'
 
 # number of Thread using in per job
 ENABLE_MULTITREADING = 1
