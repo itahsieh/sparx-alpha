2f97cfbd3b373842f7963d5916cdec300bc12c95
I-Ta Hsieh <ita.hsieh@gmail.com>
2021-05-05 10:08:43 +0800

    tiara porting (May 2021)

--- load_tiara_module.sh
+++ load_tiara_module.sh
@@ -9,17 +9,18 @@ module add python/2.7.16
 
 # The computing libraries
 module add gsl/2.5
-module add openmpi/1.10.2_ic15.0
+module add openmpi/3.1.4 
+module add fftw/3.3.8_openmpi_3.1.4
 
 # Default SPARX format: HDF
-module add hdf5/1.8.16_openmpi_1.10.2_ic15.0
+module add hdf5/1.10.5_openmpi_3.1.4
 
 # Additional dependencies
-module add intel/2015
+# module add intel/2015
 
 # The image output format library
 module add cfitsio
-module add miriad
+module add miriad/2011
 
 # redirect sparx to cluster version
 HOSTNAME=`hostname`
